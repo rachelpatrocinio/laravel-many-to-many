fb1b7c7945e3dfcc0e936136ef3327e4f9faf91e
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-06-10 20:04:37 +0200

    Added: flex shrink 0 on footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="main-footer d-flex align-items-center px-3 justify-content-between mt-auto">
+<footer class="main-footer d-flex align-items-center px-3 justify-content-between mt-auto flex-shrink-0">
     <p class="m-0"><a target="_blank" href="mailto:rachelannepatrocinio@gmail.com">rachelannepatrocinio@gmail.com</a></p>
     <p class="m-0">&copy; Rachel Patrocinio</p>
 </footer>
